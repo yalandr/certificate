6b01b59c8a41230259878c42e4d671531704d790
yalandr <52127093+yalandr@users.noreply.github.com>
2020-08-20 22:10:55 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//==== Search Field Appearence ==========================
+
+let searchIcon = document.getElementById('search__icon');
+let searchField = document.getElementById('search__field');
+
+searchIcon.onclick = () => {
+	searchField.style.display = 'block';
+};
